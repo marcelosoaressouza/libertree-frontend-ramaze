f610198c54a04395b50d3e5fd1b3efd98fbae332
rekado <rekado@elephly.net>
2012-08-13 21:01:25 +0800

    river navigation on single post view

--- helper/member.rb
+++ helper/member.rb
@@ -1,18 +1,18 @@
 module Ramaze
   module Helper
     module Member
-      def member_img(member, styles="avatar")
+      def member_img(member, styles="avatar", title=nil)
         if member.avatar_path && File.exists?(File.join(options.roots.first, options.publics.first, member.avatar_path))
           path = member.avatar_path
         else
           path = '/images/avatar-default.png'
         end
-        %|<img src="#{path}" class="#{styles}" alt="#{member.name_display}" title="#{member.name_display}" data-member-id="#{member.id}"/>|
+        %|<img src="#{path}" class="#{styles}" alt="#{member.name_display}" title="#{title || member.name_display}" data-member-id="#{member.id}"/>|
       end
 
-      def member_avatar_link(member)
+      def member_avatar_link(member, title=nil)
         return  if member.nil?
-        %|<a href="/profiles/show/#{member.id}">#{member_img(member)}</a>|
+        %|<a href="/profiles/show/#{member.id}">#{member_img(member, "avatar", title)}</a>|
       end
 
       def member_name_link(member)
